c9c0796e264c048bfeba5904b39fca541e17464c
root <root@affinytix4.(none)>
2015-11-30 13:36:40 +0100

    lots of changes

--- start_ipython_notebook.sh
+++ start_ipython_notebook.sh
@@ -4,4 +4,4 @@ YARN_CONF_DIR=/etc/hadoop/conf/
 PYSPARK_PYTHON=/opt/rh/python27/root/usr/bin/python2.7
 PYTHONPATH=/opt/rh/python27/root/usr/lib64/
 IPYTHON=1
-IPYTHON_OPTS="notebook  --ip='*' --port=80" pyspark  --master yarn-client --num-executors 3 
+IPYTHON_OPTS="notebook  --ip='*' --port=80" pyspark  --master yarn-client --num-executors 4 
